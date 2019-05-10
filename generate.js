2039fe93f39f9440657e7b05cb59639c7134b212
Drastic <demin85@ukr.net>
2019-05-10 17:46:57 +0300

    Create generate.js

---
+++ generate.js
@@ -0,0 +1,10 @@
+function generate() {
+  let weightFromText = $('#gen1').val();
+  let weightToText = $('#gen2').val();
+  let nodesText = $('#gen3').val();
+  let correlationText = $('#gen4').val();
+  let lineWeightFromText = $('#gen5').val();
+  let lineWeightToText = $('#gen6').val();
+
+  let message = "";
+}
